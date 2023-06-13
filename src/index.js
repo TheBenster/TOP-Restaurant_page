f55bf1531bef493c472a7d37d0c3d7b164ab0406
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-06-13 14:27:57 -0400

    add header and mission hero including styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import restaurant from './restaurant.jpg';
 import _ from 'lodash'
-import css from './style.css';
+import './style.css';
 
 function component(){
     const content = document.querySelector('#content')
     const element = document.createElement('div');
+    const header = document.createElement('div');
+    const restName = document.createElement('div');
+    const directHead = document.createElement('div');
+    const thaGit = document.createElement('div')
+    const home = document.createElement('div');
+    const menu = document.createElement('div');
+    const contact = document.createElement('div')
+
+    restName.textContent = "Cracker Jack's Sea Shack";
+
+    home.classList.add('guides')
+    menu.classList.add('guides')
+    contact.classList.add('guides')
+
+    // Navbar Items
+    home.textContent = "Home";
+    menu.textContent = "Menu";
+    contact.textContent = "Contact";
+    thaGit.textContent = "yds"
+    directHead.appendChild(home);
+    directHead.appendChild(menu);
+    directHead.appendChild(contact);
+    restName.classList.add('head1')
+    directHead.classList.add('head2')
+    thaGit.classList.add('head3')
+    header.classList.add('header')
+    content.appendChild(header)
+    header.appendChild(restName);
+    header.appendChild(directHead);
+    header.appendChild(thaGit);
+
+    // pic after header
     const pic = new Image();
     pic.classList.add('pic')
     pic.src = restaurant;
-    element.innerHTML = _.join(['Hello', 'TEST TEST'], ' ')
-    element.appendChild(pic)
     element.classList.add('pic')
-    content.appendChild(element)
-    return element, content
+
+    const mission = document.createElement('div')
+    
+    mission.classList.add('mission')
+
+    const pictureDiv = document.createElement('div');
+    const missionDiv = document.createElement('div');
+
+    pictureDiv.classList.add('mission-item')
+    missionDiv.classList.add('mission-item', 'mission-item2')
+
+    pictureDiv.appendChild(pic)
+    missionDiv.innerHTML = "At Cracker Jacks Sea Shack, our mission is to transport our guests to a coastal paradise where they can indulge in the flavors of the sea. <br /> <br />We strive to create a memorable dining experience by offering a vibrant and relaxed atmosphere, impeccable service, and a menu bursting with fresh, delicious seafood."
+
+    mission.appendChild(pictureDiv)
+    mission.appendChild(missionDiv)
+
+    content.appendChild(mission)
+
+    const homeHero = document.createElement('div')
+    homeHero.classList.add('homeHero');
+    content.appendChild(homeHero)
+
+    return content
 }
 document.body.appendChild(component());
